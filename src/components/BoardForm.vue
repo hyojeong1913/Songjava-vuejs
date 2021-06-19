d13c79623f10a59665c968f21915d07c5d7ee677
LEE HYO JEONG <hyojeong1913@naver.com>
2021-06-20 00:10:44 +0900

    component 분리 및 URL mapping 마무리 및 bootstrap 적용

--- src/components/BoardForm.vue
+++ src/components/BoardForm.vue
@@ -51,20 +51,6 @@ export default {
 }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+<style>
+    @import "https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css";
 </style>
